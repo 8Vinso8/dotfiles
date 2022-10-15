21857e4ef49a840466a1ed8d5729fddec0b8c009
Vinso <8vinso8@gmail.com>
2022-10-16 00:04:15 +1000

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,3 @@
-### ADDING TO THE PATH
-# First line removes the path; second line sets it.  Without the first line,
-# your path gets massive and fish becomes very slow.
 set -e fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
 
@@ -10,9 +7,38 @@ alias vim='nvim'
 
 alias pacsyu='sudo pacman -Syu'
 alias mirrors='sudo reflector --country Japan --sort rate --verbose --save /etc/pacman.d/mirrorlist'
-alias yaysyu='yay -Syu'
+alias yaysua='yay -Sua --noconfirm'
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
 
 alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
 
 colorscript random
 
