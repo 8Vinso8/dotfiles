3aebecafab76da08ab99805f926b3b63062cba8e
Vinso <8vinso8@gmail.com>
2025-09-14 09:28:29 +1000

    more config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,11 +6,11 @@ function starship_transient_prompt_func
     starship module character
 end
 
-if status is-login
-    if test (tty) = /dev/tty1; and uwsm check may-start
-        uwsm start niri.desktop
-    end
-end
+#if status is-login
+#    if test (tty) = /dev/tty1; and uwsm check may-start
+#        uwsm start niri.desktop
+#    end
+#end
 if status is-interactive
     alias dotfiles '/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
     starship init fish | source
